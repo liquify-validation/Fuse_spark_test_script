dfa43a47639d4d1fe8811506e1d8f76f13a0ea88
Andrew-Pohl <55916961+Andrew-Pohl@users.noreply.github.com>
2020-09-24 17:00:04 +0100

    Addressed issue 5 and 6 (#7)
    * addressed issue 5 and 6
    
    • Made the validator in the cycle data a nested dict to make it easier to follow
    • Implemented mid cycle stake changes
    • Latest data
    • Updated jsonExporter to work with new file format
    • Added a scatter of diff
    • Removed commented code and pie charts-pie charts aren't to useful so removed

--- jsonExport.py
+++ jsonExport.py
@@ -1,13 +1,13 @@
 import json
 import matplotlib.pyplot as plt
 import csv
+import sys
 
 with open("data/results.json") as json_file:
     data = json.load(json_file)
 
 with open("data/cycleData.json") as json_file:
     cycledata = json.load(json_file)
-
 
 graph = {}
 
@@ -17,6 +17,7 @@ for i in data:
         graph[data[i]['miner']['node']]['startBalance'] = data[i]['miner']['balanceBefore']
         graph[data[i]['miner']['node']]['x'] = []
         graph[data[i]['miner']['node']]['y'] = []
+        graph[data[i]['miner']['node']]['diff'] = []
         graph[data[i]['miner']['node']]['offset'] = 0.0
 
 
@@ -28,6 +29,7 @@ for i in data:
         result = data[i]['miner']['balanceNow']-graph[data[i]['miner']['node']]['startBalance']+graph[data[i]['miner']['node']]['offset']
 
     graph[data[i]['miner']['node']]['y'].append(result)
+    graph[data[i]['miner']['node']]['diff'].append(data[i]['miner']['diff'])
 
     for dele in data[i]['delegator']:
         delegatorStr = dele+'-delegate'
@@ -36,6 +38,7 @@ for i in data:
             graph[delegatorStr]['startBalance'] = data[i]['delegator'][dele]['balanceBefore']
             graph[delegatorStr]['x'] = []
             graph[delegatorStr]['y'] = []
+            graph[delegatorStr]['diff'] = []
             graph[delegatorStr]['offset'] = 0.0
         graph[delegatorStr]['x'].append(int(i))
         result = data[i]['delegator'][dele]['balanceNow'] - graph[delegatorStr]['startBalance'] + graph[delegatorStr]['offset']
@@ -44,48 +47,56 @@ for i in data:
 
             result = data[i]['delegator'][dele]['balanceNow'] - graph[delegatorStr]['startBalance'] + graph[delegatorStr]['offset']
         graph[delegatorStr]['y'].append(result)
+        graph[delegatorStr]['diff'].append(data[i]['delegator'][dele]['diff'])
 
+lowestX = sys.maxsize
+highestX = 0
 
 for key in graph:
     plt.plot(graph[key]['x'], graph[key]['y'], label=key)
+    if graph[key]['x'][0] < lowestX:
+        lowestX = graph[key]['x'][0]
+    if graph[key]['x'][-1] > highestX:
+        highestX = graph[key]['x'][-1]
 
 plt.legend()
 plt.show()
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+for key in graph:
+    ax1.scatter(graph[key]['x'],graph[key]['diff'],s=1, label=key)
+
+
+plt.xlim(left=lowestX)
+plt.xlim(right=highestX)
+plt.ylim(bottom=0)
+plt.ylim(top=5)
+plt.legend()
+plt.show()
+
 
 cycleGraph = {}
 pieChart = {}
 
 
 for i in cycledata:
-    pieChart[i] = {}
-    pieChart[i]['nodeName'] = []
-    pieChart[i]['nodeShare'] = []
     plt.plot(x=cycledata[i]['startBlock'], color='gray')
-    for val in cycledata[i]:
-        if type(cycledata[i][val]) is dict:
-            if val not in cycleGraph:
-                cycleGraph[val] = {}
-                cycleGraph[val]['x'] = []
-                cycleGraph[val]['y'] = []
-            cycleGraph[val]['x'].append(cycledata[i]['startBlock'])
-            cycleGraph[val]['y'].append(cycledata[i][val]['rewardPerBlock'])
-            cycleGraph[val]['x'].append(cycledata[i]['endBlock'])
-            cycleGraph[val]['y'].append(cycledata[i][val]['rewardPerBlock'])
-            pieChart[i]['nodeName'].append(val)
-            pieChart[i]['nodeShare'].append(cycledata[i][val]['rewardToNode'])
-            pieChart[i]['title'] = "cycle " + str(cycledata[i]['startBlock']) + " to " + str(cycledata[i]['endBlock']) + " ( " + str(cycledata[i]['cycleLength']) + " blocks )"
-
-# skip first X pie charts
-skipPieCharts = 80
-for key in pieChart:
-    if (int(key) > skipPieCharts):
-        plt.pie(pieChart[key]['nodeShare'], labels=pieChart[key]['nodeName'],
-            autopct='%1.1f%%', startangle=140)
-
-        plt.title(pieChart[key]['title'])
-        plt.axis('equal')
-        plt.savefig('pieCharts/'+ pieChart[key]['title'] + '.jpeg', dpi=300, bbox_inches='tight')
-        plt.clf()
+    for subCycle in cycledata[i]['subCycleData']:
+        for val in cycledata[i]['subCycleData'][subCycle]['validators']:
+            if type(cycledata[i]['subCycleData'][subCycle]['validators'][val]) is dict:
+                if val not in cycleGraph:
+                    cycleGraph[val] = {}
+                    cycleGraph[val]['x'] = []
+                    cycleGraph[val]['y'] = []
+                cycleGraph[val]['x'].append(cycledata[i]['subCycleData'][subCycle]['startBlock'])
+                cycleGraph[val]['y'].append(cycledata[i]['subCycleData'][subCycle]['validators'][val]['rewardPerBlock'])
+                cycleGraph[val]['x'].append(cycledata[i]['subCycleData'][subCycle]['endBlock'])
+                cycleGraph[val]['y'].append(cycledata[i]['subCycleData'][subCycle]['validators'][val]['rewardPerBlock'])
 
 for key in cycleGraph:
     plt.plot(cycleGraph[key]['x'], cycleGraph[key]['y'], label=key)
+
+plt.legend()
+plt.show()
