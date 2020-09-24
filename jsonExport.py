eeabdd0de80ff073ee443141eb24595f50aae48a
Andrew Pohl <andypohl@hotmail.co.uk>
2020-09-24 13:05:39 +0100

    Added a scatter of diff

--- jsonExport.py
+++ jsonExport.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import csv
+import sys
 
 with open("data/results.json") as json_file:
     data = json.load(json_file)
@@ -35,6 +36,7 @@ for i in data:
         graph[data[i]['miner']['node']]['startBalance'] = data[i]['miner']['balanceBefore']
         graph[data[i]['miner']['node']]['x'] = []
         graph[data[i]['miner']['node']]['y'] = []
+        graph[data[i]['miner']['node']]['diff'] = []
         graph[data[i]['miner']['node']]['offset'] = 0.0
 
 
@@ -46,6 +48,7 @@ for i in data:
         result = data[i]['miner']['balanceNow']-graph[data[i]['miner']['node']]['startBalance']+graph[data[i]['miner']['node']]['offset']
 
     graph[data[i]['miner']['node']]['y'].append(result)
+    graph[data[i]['miner']['node']]['diff'].append(data[i]['miner']['diff'])
 
     for dele in data[i]['delegator']:
         delegatorStr = dele+'-delegate'
@@ -54,6 +57,7 @@ for i in data:
             graph[delegatorStr]['startBalance'] = data[i]['delegator'][dele]['balanceBefore']
             graph[delegatorStr]['x'] = []
             graph[delegatorStr]['y'] = []
+            graph[delegatorStr]['diff'] = []
             graph[delegatorStr]['offset'] = 0.0
         graph[delegatorStr]['x'].append(int(i))
         result = data[i]['delegator'][dele]['balanceNow'] - graph[delegatorStr]['startBalance'] + graph[delegatorStr]['offset']
@@ -62,13 +66,35 @@ for i in data:
 
             result = data[i]['delegator'][dele]['balanceNow'] - graph[delegatorStr]['startBalance'] + graph[delegatorStr]['offset']
         graph[delegatorStr]['y'].append(result)
+        graph[delegatorStr]['diff'].append(data[i]['delegator'][dele]['diff'])
 
+lowestX = sys.maxsize
+highestX = 0
 
 for key in graph:
     plt.plot(graph[key]['x'], graph[key]['y'], label=key)
+    if graph[key]['x'][0] < lowestX:
+        lowestX = graph[key]['x'][0]
+    if graph[key]['x'][-1] > highestX:
+        highestX = graph[key]['x'][-1]
 
 plt.legend()
 plt.show()
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+for key in graph:
+    ax1.scatter(graph[key]['x'],graph[key]['diff'],s=1, label=key)
+
+
+plt.xlim(left=lowestX)
+plt.xlim(right=highestX)
+plt.ylim(bottom=0)
+plt.ylim(top=5)
+plt.legend()
+plt.show()
+
 
 cycleGraph = {}
 pieChart = {}
